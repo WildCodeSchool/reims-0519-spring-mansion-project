57b825831763da55fea15f76baa1278f782c2be3
Rémy Trohel <remy.trohel@gmail.com>
2019-07-02 12:04:35 +0200

    controller for game page updated

--- src/main/java/com/wildcodeschool/reims0519springmansionproject/controllers/ControllerGame.java
+++ src/main/java/com/wildcodeschool/reims0519springmansionproject/controllers/ControllerGame.java
@@ -1,12 +1,12 @@
 package com.wildcodeschool.reims0519springmansionproject.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ControllerGame {
     
-    @GetMapping("/Game")
+    @GetMapping("/game")
     public String getGame() {
         return "";
     }
