d0ea2b58f7cd7d1410f5ea99643cf6150c3508c4
Lea Goarant <leagoarant@msn.com>
2019-07-02 15:35:32 +0200

    Remplissage initial, fichier java

--- src/main/java/com/wildcodeschool/reims0519springmansionproject/entities/Characters.java
+++ src/main/java/com/wildcodeschool/reims0519springmansionproject/entities/Characters.java
@@ -1,3 +1,30 @@
 class Characters{
+    private String name;
+    private String dialogue;
+    
+    public String Watson(String name, String dialogue){
+        this.name = name;
+        this.dialogue = dialogue;
+
+        // Insérer, si on est dans le Hall, Watson dit //
+
+        return "Ecrire indice";
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDialogue() {
+		return dialogue;
+	}
+
+	public void setDialogue(String dialogue) {
+		this.dialogue = dialogue;
+	}
     
 }
