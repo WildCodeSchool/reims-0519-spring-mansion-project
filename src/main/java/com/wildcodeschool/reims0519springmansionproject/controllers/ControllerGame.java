c79a40253887d84d62c82e896b7f23c9d6276708
Rémy Trohel <remy.trohel@gmail.com>
2019-07-02 14:39:34 +0200

    room route created

--- src/main/java/com/wildcodeschool/reims0519springmansionproject/controllers/ControllerGame.java
+++ src/main/java/com/wildcodeschool/reims0519springmansionproject/controllers/ControllerGame.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.reims0519springmansionproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ControllerGame {
@@ -9,5 +10,10 @@ public class ControllerGame {
     @GetMapping("/game")
     public String getGame() {
         return "";
+    }
+
+    @GetMapping("/game/{id}")
+    public Room getRoom(@PathVariable int id, Player player) {
+        return player.getMansion().get(id);
     }
 }
