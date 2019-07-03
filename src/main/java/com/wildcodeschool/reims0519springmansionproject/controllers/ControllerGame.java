05e69c51aabcc213b7fd01e52ffb35441fc7f278
Rémy Trohel <remy.trohel@gmail.com>
2019-07-03 14:33:26 +0200

    route updated to navigate through rooms

--- src/main/java/com/wildcodeschool/reims0519springmansionproject/controllers/ControllerGame.java
+++ src/main/java/com/wildcodeschool/reims0519springmansionproject/controllers/ControllerGame.java
@@ -1,19 +1,33 @@
 package com.wildcodeschool.reims0519springmansionproject.controllers;
 
+import java.util.ArrayList;
+
+import com.wildcodeschool.reims0519springmansionproject.entities.Room;
+import com.wildcodeschool.reims0519springmansionproject.repositories.RoomRepository;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ControllerGame {
-    
+
+    private static RoomRepository mansion = new RoomRepository();
+
     @GetMapping("/game")
-    public String getGame() {
-        return "";
+    public String getRoomZero() {
+        return "redirect:/game/0";
     }
 
-    @PostMapping("/game")
-    public String postRoom() {
-        return "";
+    @GetMapping("/game/{id}")
+    public String getRoom(Model model, @PathVariable int id) {
+        ArrayList<Room> myList = new ArrayList<Room>();
+        for (Integer roomId : mansion.getRoomById(id).getAdjacentRooms()) {
+            Room room = mansion.getRoomById(roomId);
+            myList.add(room);
+        }
+        model.addAttribute("rooms", myList);
+        return "gamepage";
     }
 }
