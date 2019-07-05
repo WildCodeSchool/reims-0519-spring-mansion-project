c1e8947e0975c29a8c95bf7e499f4a03eb8bab54
Rémy Trohel <remy.trohel@gmail.com>
2019-07-05 10:13:12 +0200

    score class and repository updated

--- src/main/java/com/wildcodeschool/reims0519springmansionproject/entities/Score.java
+++ src/main/java/com/wildcodeschool/reims0519springmansionproject/entities/Score.java
@@ -1,17 +1,57 @@
 package com.wildcodeschool.reims0519springmansionproject.entities;
 
 public class Score {
-    private long time;
+    private int id;
+    private String name;
+    private long initialTime;
+    private String timeString;
 
     public Score(){
-        this.time = System.currentTimeMillis();
-    }
-    
-    public long getTime() {
-        return time;
+        this.initialTime = System.currentTimeMillis();
     }
 
-    public void setTime(long time) {
-        this.time = time;
+    public Score(int id, String name, String timeString) {
+        this.id = id;
+        this.name = name;
+        this.timeString = timeString;
+    }
+    
+    public long getEllapsedTime() {
+        return System.currentTimeMillis() - initialTime;
+    }
+
+    public void setInitialTime(long time) {
+        this.initialTime = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeString() {
+        return timeString;
+    }
+
+    public void setTimeString(String timeString) {
+        this.timeString = timeString;
+    }
+
+    public static String formatTime(long time) {
+        long seconds = (time % 60);
+        long minutes = (time % 3600)/60;
+        long hours = (time / 3600);
+        return hours + "h " + minutes + "m "+ seconds + "s ";
     }
 }
---
+++ src/main/java/com/wildcodeschool/reims0519springmansionproject/repositories/ScoreRepository.java
@@ -0,0 +1,125 @@
+package com.wildcodeschool.reims0519springmansionproject.repositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wildcodeschool.reims0519springmansionproject.entities.Score;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class ScoreRepository {
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/mansion_project?serverTimezone=GMT";
+    private final static String DB_USER = "javaclues";
+    private final static String DB_PASSWORD = "Wcsmansion2019!";
+
+    public static List<Score> selectAll() {
+        try (
+            Connection connection = DriverManager.getConnection(
+                DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM score"
+            );
+        ) {
+            try (
+                ResultSet resultSet = statement.executeQuery();
+            ) {
+                ArrayList<Score> scores = new ArrayList<Score>();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    long time = resultSet.getLong("time");
+                    String timeString = Score.formatTime(time);
+                    scores.add(new Score(id, name, timeString));
+                }
+                return scores;
+            }
+        } catch (SQLException e) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
+        }
+    }
+
+    public static int insert(
+        String name,
+        long time
+    ) {
+        try (
+            Connection connection = DriverManager.getConnection(
+                DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO score (name, time) VALUES (?, ?)",
+                Statement.RETURN_GENERATED_KEYS
+            );
+        ) {
+            statement.setString(1, name);
+            statement.setLong(2, time);
+
+            if (statement.executeUpdate() != 1) {
+                throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "failed to insert data"
+                );
+            }
+
+            try (
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+            ) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                }
+                else {
+                    throw new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "failed to get inserted id"
+                    );
+                }
+            }
+        } 
+        catch (SQLException e) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
+        }
+    }
+
+    public static Score selectById(int id) {
+        try (
+            Connection connection = DriverManager.getConnection(
+                DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM score WHERE id=?"
+            );
+        ) {
+            statement.setInt(1, id);
+            try (
+                ResultSet resultSet = statement.executeQuery();
+            ) {
+                if (resultSet.next()) {
+                    String name = resultSet.getString("name");
+                    long time = resultSet.getLong("time");
+                    String timeString = Score.formatTime(time);
+                    return new Score(id, name, timeString);
+                }
+                else {
+                    throw new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "unknown id in table score"
+                    );
+                }
+            }
+        } 
+        catch (SQLException e) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
+        }
+    }
+}
